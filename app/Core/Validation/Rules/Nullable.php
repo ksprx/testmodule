5f22d9d85e274fe804f9a4b4b9b4c7f277c602ff
Mohamad  Dadfar <mohamad.liamg77@gmail.com>
2025-01-16 20:43:15 +0330

    Add Nullable Validation

--- app/Core/Validation/Rules/Nullable.php
+++ app/Core/Validation/Rules/Nullable.php
@@ -8,11 +8,23 @@ class Nullable implements RuleContract
 {
     public function validate($attribute, $value, $parameters = []): bool
     {
-        //TODO
+        if (is_null($value) || $value === '') {
+            return true;
+        }
+
+        if (is_array($value) && empty($value)) {
+            return true;
+        }
+
+        if (is_object($value) && empty(get_object_vars($value))) {
+            return true;
+        }
+
+        return true;
     }
 
     public function message($attribute, $parameters = []): string
     {
-        //TODO
+        return "The {$attribute} field is optional and can be null or empty.";
     }
 }
