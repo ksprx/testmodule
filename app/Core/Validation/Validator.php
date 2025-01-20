c21ffa327da43f55d2c66b7c337886ca3a8a394e
Mohamad  Dadfar <mohamad.liamg77@gmail.com>
2025-01-21 02:05:33 +0330

    Validate from custom data

--- app/Core/Validation/Validator.php
+++ app/Core/Validation/Validator.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Validation;
-
 namespace App\Core\Validation;
 
 use App\Core\Request\Request;
@@ -21,45 +19,83 @@ class Validator
         foreach ($rules as $field => $ruleData) {
             if (is_string($ruleData)) {
                 $rules = explode('|', $ruleData);
-
             } elseif (is_array($ruleData)) {
                 $rules = $ruleData;
             }
             foreach ($rules as $rule) {
-                if (!empty($rule))
+                if (!empty($rule)) {
                     $this->handleRule($field, $rule);
+                }
             }
         }
 
         return empty($this->errors);
     }
 
+    public function validateData(array $rules, array $data): bool
+    {
+        foreach ($rules as $field => $ruleData) {
+            if (is_string($ruleData)) {
+                $rulesList = explode('|', $ruleData);
+            } elseif (is_array($ruleData)) {
+                $rulesList = $ruleData;
+            }
+            foreach ($rulesList as $rule) {
+                if (!empty($rule)) {
+                    $this->handleRuleWithData($field, $rule, $data);
+                }
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    protected function handleRuleWithData(string $field, $rule, array $data): void
+    {
+        $ruleInstance = $this->resolveRule($rule);
+
+        $value = $data[$field] ?? null;
+
+        if (!$ruleInstance->validate($field, $value)) {
+            $this->errors[$field][] = $ruleInstance->message($field);
+        }
+    }
+
     protected function handleRule(string $field, $rule): void
     {
-        $ruleInstance = null;
+        $ruleInstance = $this->resolveRule($rule);
 
+        $value = $this->request->input($field);
+
+        if (!$ruleInstance->validate($field, $value)) {
+            $this->errors[$field][] = $ruleInstance->message($field);
+        }
+    }
+
+    protected function resolveRule($rule)
+    {
         if (is_string($rule)) {
             $parts = explode(':', $rule);
             $ruleName = $parts[0];
             $parameters = isset($parts[1]) ? explode(',', $parts[1]) : [];
-            $ruleClass = $this->getRuleClass($ruleName);
 
-            $ruleInstance = new $ruleClass(...$parameters);
-        } elseif (is_array($rule)) {
+            $ruleClass = $this->getRuleClass($ruleName);
+            return new $ruleClass(...$parameters);
+        }
+
+        if (is_array($rule)) {
             $ruleClass = $rule[0];
             $parameters = $rule[1] ?? [];
 
-            $ruleInstance = new $ruleClass(...$parameters);
+            return new $ruleClass(...$parameters);
         }
-        if (!$ruleInstance->validate($field, $this->request->input($field))) {
-            $this->errors[$field][] = $ruleInstance->message($field);
-        }
+
+        throw new \Exception("Invalid rule format.");
     }
 
     protected function getRuleClass(string $ruleName): string
     {
         $ruleName = ucwords(str_replace('_', '', $ruleName));
-
         $ruleClass = "App\\Core\\Validation\\Rules\\{$ruleName}";
 
         if (class_exists($ruleClass)) {
